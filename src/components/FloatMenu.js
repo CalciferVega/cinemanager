35ae8bc9e1d208ef49bf61f7ceb69939f05c17b9
EL <el@MacBook-Pro-de-EL.local>
2022-06-19 16:17:42 -0500

    menu repair

--- src/components/FloatMenu.js
+++ src/components/FloatMenu.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 export default function FloatMenu() {
   return (
-    <div className="menu-container sticky bottom-0 pt-3">
+    <div className="menu-container fixed bottom-0 pt-3">
       <nav className="float-menu flex ml-4 mr-4 mb-4 mt-6">
         <Link to="/"><img src="assets/home.svg" alt="home" /></Link>
         <Link to="/menu"><img src="assets/menu.svg" alt="menu" /></Link>
